c85280b69633df0793e6c51c544b5aa428f52e28
Manuel Bote <mbotez01@educarex.es>
2023-09-24 21:16:32 +0200

    ej8

---
+++ Unidad 1/Ej8/js/script2.js
@@ -0,0 +1,42 @@
+window.onload=inicio;
+
+function inicio(){
+
+    let btn=document.getElementById("mostrarJS");
+    let cajaPadre=document.getElementsByClassName("container");
+    let imagenes=document.getElementById("cajaImagenes");
+
+    btn.onclick=generar;
+
+    function generar(){
+        console.log("Entrar en la funcion generar");
+
+        imagenes.innerHTML="";
+
+        for(let i =0;i<20;i++){
+            
+
+            let caja=document.createElement("img");
+            //caja.className("gallery img");
+
+            let num;
+
+            do{
+                num = Math.round(Math.random()*190);
+            } while(num==0);
+
+            if(num>95){
+                num= num-95;
+                caja.src= "https://randomuser.me/api/portraits/women/"+num+".jpg";
+                imagenes.appendChild(caja);
+            } else {
+                caja.src= "https://randomuser.me/api/portraits/men/"+num+".jpg";
+                imagenes.appendChild(caja);
+            }
+        }
+
+    }
+
+
+
+}
