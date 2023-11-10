4ac23258b87d6c28e9b28ac9b1cf63662411eeee
Manuel Bote <mbotez01@educarex.es>
2023-11-10 12:28:33 +0100

    CRUD clientes

--- Unidad 2/AJAX/Ej5 CRUD Modal/script.js
+++ Unidad 2/AJAX/Ej5 CRUD Modal/script.js
@@ -14,7 +14,10 @@ function inicio(){
         var apellido = document.querySelector("#Apellidos").setAttribute("value", "");
         var telefono = document.querySelector("#Telefono").setAttribute("value", "");
     });
+
     document.querySelector("#btnInsertar").addEventListener("click", insertarCLientes);
+
+    let btnMostrar = document.querySelector("#btnMostrar").addEventListener("click", cargarT);
 
 
     cargarT();
@@ -51,8 +54,13 @@ function cargarT(){
                     "<td>" + dato.nombre + "</td>" +
                     "<td>" + dato.apellido + "</td>" +
                     "<td>" + dato.telefono + "</td>" +
-                    "<td> <button type='button' class='btn btn-primary btnMod' data-toggle='modal' data-target='#clienteModal'>Modificar</button></td>" +
-                    "<td> <button type='button' class='btn btn-danger btnBorrar'>Borrar</button></td>";
+                    "<td> <button type='button' class='btn btn-primary btnMod' data-toggle='modal' data-target='#clienteModal'>&#x270e Modificar</button></td>" +
+                    "<td> <button type='button' class='btn btn-danger btnBorrar' data-toggle='modal' data-target='#modalBorrar'>"+
+                    "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>"+
+                    "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z'/>"+
+                    "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z'/>"+
+                    "</svg>"+
+                    " Borrar</button></td>";
             
                 tabla.appendChild(fila);
             
@@ -66,7 +74,48 @@ function cargarT(){
                     var apellido = document.querySelector("#Apellidos").setAttribute("value", dato.apellido);
                     var telefono = document.querySelector("#Telefono").setAttribute("value", dato.telefono);
 
+                    document.querySelector("#btnModificar").addEventListener("click", function(){
+
+                        var dni= document.querySelector("#Dni").value;
+                        var nombre= document.querySelector("#Nombre").value;
+                        var apellido= document.querySelector("#Apellidos").value;
+                        var telefono= document.querySelector("#Telefono").value;
+
+                        $.ajax({
+                            url: "http://moralo.atwebpages.com/menuAjax/clientes/modificarClientes.php",
+                            type: "POST",
+                            data:{
+                                dni:dni,
+                                nombre:nombre,
+                                apellido:apellido,
+                                telefono:telefono
+                            }
+                        });
+
+                        cargarT();
+                    })
                 });
+
+                let borrar = fila.querySelector(".btnBorrar").addEventListener("click", function(){
+                    let mBody = document.querySelector("#borrarBody").innerHTML= "¿Estás seguro de que desea borrar el cliente <b>"+dato.nombre+"</b> con dni: <b>" + dato.dni+"</b>?";
+                    document.querySelector("#borrarCliente").addEventListener("click", function(){
+
+                        var dni= dato.dni;
+                        //console.log(dni);
+
+                        $.ajax({
+                            url: "http://moralo.atwebpages.com/menuAjax/clientes/eliminarClientes.php",
+                            type: "POST",
+                            data:{
+                                dni:dni
+                            },
+                        });
+                        
+                    
+                        cargarT();
+                    })
+                })
+
             }
             
         }
