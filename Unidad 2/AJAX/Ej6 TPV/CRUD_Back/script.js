5a22dfaadb1df559239e1f445b3dc1914d444b49
Manuel Bote <mbotez01@educarex.es>
2023-11-20 13:36:29 +0100

    front-back

---
+++ Unidad 2/AJAX/Ej6 TPV/CRUD_Back/script.js
@@ -0,0 +1,190 @@
+window.onload = inicio;
+
+var tabla = document.querySelector("#cajaTabla");
+
+function inicio() {
+  //Boton añadir
+  let btnI = document
+    .querySelector("#botonAdd")
+    .addEventListener("click", function () {
+      document.querySelector("#btnInsertar").disabled = false;
+      document.querySelector("#btnModificar").disabled = true;
+      var id = document.querySelector("#Id");
+      id.setAttribute("value", "");
+      id.disabled = false;
+      var name = document.querySelector("#Nombre").setAttribute("value", "");
+      var price = document
+        .querySelector("#Precio")
+        .setAttribute("value", "");
+      var photo = document
+        .querySelector("#URL")
+        .setAttribute("value", "");
+    });
+
+  //Boton insertar (Modal)
+  document
+    .querySelector("#btnInsertar")
+    .addEventListener("click", insertarCLientes);
+
+  //Boton mostrar(A veces recarga tan rapido que no se guarda la modificacion realizada)
+  let btnMostrar = document
+    .querySelector("#btnMostrar")
+    .addEventListener("click", cargarT);
+
+  cargarT();
+}
+
+//Funcion cargar de la tabla
+function cargarT() {
+  tabla.innerHTML = "";
+
+  var filaH = document.createElement("tr");
+  filaH.innerHTML =
+    "<th scope='col'>ID</th>" +
+    "<th scope='col'>Nombre</th>" +
+    "<th scope='col'>Precio</th>" +
+    "<th scope='col'>Imagen</th>" +
+    "<th scope='col'>Modificar</th>" +
+    "<th scope='col'>Eliminar</th>";
+  tabla.appendChild(filaH);
+
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = cargar;
+
+  function cargar() {
+    if (this.readyState == 4 && this.status == 200) {
+      var objeto = JSON.parse(this.responseText);
+
+      objeto.forEach(recorrer);
+
+      function recorrer(dato, indice) {
+        var fila = document.createElement("tr");
+        //Añadida de objetos
+        fila.innerHTML =
+          "<th scope='row'>" +
+          dato.id +
+          "</th>" +
+          "<td>" +
+          dato.name +
+          "</td>" +
+          "<td>" +
+          dato.price +
+          "€</td>" +
+          "<td><img src='" +
+          dato.photo +
+          "' width='40px'/></td>" +
+          "<td> <button type='button' class='btn btn-primary btnMod' data-toggle='modal' data-target='#clienteModal'>&#x270e Modificar</button></td>" +
+          "<td> <button type='button' class='btn btn-danger btnBorrar' data-toggle='modal' data-target='#modalBorrar'>" +
+          "<svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash' viewBox='0 0 16 16'>" +
+          "<path d='M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z'/>" +
+          "<path d='M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z'/>" +
+          "</svg>" +
+          " Borrar</button></td>";
+
+        tabla.appendChild(fila);
+
+        //Boton modificar
+        let mod = fila
+          .querySelector(".btnMod")
+          .addEventListener("click", function () {
+            document.querySelector("#btnInsertar").disabled = true;
+            document.querySelector("#btnModificar").disabled = false;
+            var id = document.querySelector("#Dni");
+            id.setAttribute("value", dato.id);
+            id.disabled = true;
+            var name = document
+              .querySelector("#Nombre")
+              .setAttribute("value", dato.name);
+            var price = document
+              .querySelector("#Apellidos")
+              .setAttribute("value", dato.price);
+            var photo = document
+              .querySelector("#Telefono")
+              .setAttribute("value", dato.photo);
+
+            //Funcion de modificar
+            document
+              .querySelector("#btnModificar")
+              .addEventListener("click", function () {
+                var id = document.querySelector("#Id").value;
+                var name = document.querySelector("#Nombre").value;
+                var price = document.querySelector("#Precio").value;
+                var photo = document.querySelector("#URL").value;
+
+                $.ajax({
+                  url: "http://moralo.atwebpages.com/menuAjax/productos/update_product.php",
+                  type: "POST",
+                  data: {
+                    id: id,
+                    name: name,
+                    price: price,
+                    photo: photo,
+                  },
+                });
+
+                cargarT();
+              });
+          });
+
+        //Boton de borrar
+        let borrar = fila
+          .querySelector(".btnBorrar")
+          .addEventListener("click", function () {
+            let mBody = (document.querySelector("#borrarBody").innerHTML =
+              "¿Estás seguro de que desea borrar el cliente <b>" +
+              dato.name +
+              "</b> con dni: <b>" +
+              dato.id +
+              "</b>?");
+            //Funcion de borrar
+            document
+              .querySelector("#borrarCliente")
+              .addEventListener("click", function () {
+                var id = dato.id;
+                //console.log(dni);
+
+                $.ajax({
+                  url: "http://moralo.atwebpages.com/menuAjax/clientes/eliminarClientes.php",
+                  type: "POST",
+                  data: {
+                    id: id,
+                  },
+                });
+
+                cargarT();
+              });
+          });
+      }
+    }
+  }
+
+  xhr.open(
+    "POST",
+    "http://moralo.atwebpages.com/menuAjax/productos/index.php",
+    true
+  );
+  xhr.send();
+}
+
+//Funcion de insertar datos
+function insertarCLientes() {
+  console.log("Entro en insertar clientes");
+
+  var id = document.querySelector("#Id").value;
+  var name = document.querySelector("#Nombre").value;
+  var price = document.querySelector("#Precio").value;
+  var photo = document.querySelector("#URL").value;
+
+  $.ajax({
+    url: "http://moralo.atwebpages.com/menuAjax/productos/create_product.php",
+    type: "POST",
+    data: {
+      id: id,
+      name: name,
+      price: price,
+      photo: photo,
+    },
+  });
+
+  cargarT();
+}
---
+++ Unidad 2/AJAX/Ej6 TPV/CRUD_Front/script.js
@@ -0,0 +1,163 @@
+window.onload = inicio;
+let cajaFrutas = document.querySelector("#cajaFrutas");
+cajaFrutas.innerHTML = "";
+let bloqueHtml = document.createElement("div");
+let bloqueCesta = document.createElement("div");
+var objeto;
+let contenedorCesta = document.querySelector("tbody");
+var total = 0;
+var calculoPrecio = 0;
+let precioTotal = document.getElementById("precio");
+
+let pwd = document.getElementById("idPwd");
+let btnGestionAl = document.getElementById("confirmar");
+btnGestionAl.onclick = login;
+
+let btnPDF = document.getElementById("imprimirPDF");
+btnPDF.onclick = mostrarPDF;
+
+let btnEnviar = document.getElementById("enviarPDF");
+btnEnviar.onclick = enviarMail;
+
+// ------------------------------------------------------------------------------------------------------------------
+
+function inicio() {
+  cargarContenido();
+  console.log(objeto);
+}
+
+function mostrarPDF(){
+  var estilo = "<style>"+
+                "table {width: 100%;font: 17px Calibri;}"+
+                "table, th, td {border: solid 1px #DDD; border-collapse: collapse;"+
+                "padding: 2px 3px;text-align: center;}"+
+                "</style>";
+
+  let ticket1= document.getElementById("cestaCompra").innerHTML;
+  console.log(ticket1);
+
+  let win = window.open("ticked.pdf", "Ticket Fruteria", "height=700,width=700");
+  win.document.write('<html><head>');
+  win.document.write('<title>Ticket</title>'); //cabecera del pdf
+  win.document.write(estilo); // estilo cabecera
+  win.document.write('</head>');
+  win.document.write('<body>');
+  win.document.write("<table>");
+  win.document.write(ticket1);
+  win.document.write("</table>");
+  win.document.write("<br/>Total: "+precio.textContent); // contenidos dentro del body
+  win.document.write('</body></html>');
+  
+  win.document.close(); //cerrar ventana 
+  win.print();
+}
+
+function enviarMail(){
+    alert("enviar mail");
+    Email.send({
+    Host: "smtp.gmail.com",
+    Username: "mbotez01@educarex.es",
+    Password: "zdwclpdilqfflcdp ",
+    To: 'profeaugustobriga@gmail.com',
+    From: 'mbotez01@educarex.es',
+    Subject: "Enviar mail usuario JS",
+    Body: "TODO OK!!",
+    // Attachments: [
+    // {
+    // name : "factura.pdf",
+    // path : pdfBase64
+    // }]
+    }).then(function () {
+    alert("MAIL ENVIADO OK")
+    });
+}
+
+function cargarFrutas() {
+  bloqueHtml.className = "row";
+  for (let i = 0; i < objeto.length; i++) {
+    let vector = [];
+    vector.push(objeto[i].id, objeto[i].name, objeto[i].photo, objeto[i].price);
+    bloqueHtml.innerHTML +=
+      '<div class="col-lg-4">' +
+      '<img class="card-img-top" onclick=anadirCesta("' +
+      vector +
+      '") src=' +
+      objeto[i].photo +
+      ' width="90" height="90" alt=' +
+      objeto[i].id +
+      ">" +
+      '<div class="card-body" onclick=anadirCesta("' +
+      vector +
+      '")>' +
+      '<h4 class="card-title">' +
+      objeto[i].name +
+      "</h4>" +
+      '<p class="card-text">' +
+      objeto[i].price +
+      "</p></div></div>";
+  }
+
+  cajaFrutas.appendChild(bloqueHtml);
+}
+
+function cargarContenido() {
+  var xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      objeto = JSON.parse(this.responseText);
+      cargarFrutas();
+    }
+  };
+  xhr.open(
+    "GET",
+    "http://moralo.atwebpages.com/menuAjax/productos/index.php",
+    true
+  );
+  xhr.send();
+}
+
+function anadirCesta(vector) {
+  let cajaTr = document.createElement("tr");
+  console.log(vector);
+  let vectorX = vector.split(",");
+  let peso = prompt("Teclea los kgs de " + vectorX[1]);
+  calculoPrecio = peso * parseFloat(vectorX[3]);
+
+  total = total + calculoPrecio;
+  precioTotal.textContent = total + "€";
+
+  if (peso && !isNaN(peso)) {
+    cajaTr.innerHTML =
+      "<td>"+vectorX[1] +"</td>"+
+      "<td>"+vectorX[3]+"€</td>"+
+      "<td>"+peso+"Kg</td>"+
+      "<td>"+calculoPrecio +
+      "€</td>" +
+      "<td>" + //simular botón con a href añado clase btn btn-danger (color rojo)
+      "<div class='col-lg-2 text-center mb-2'><a class='btn btn-danger btn-md'" +
+      //anulo el href, no hay link , pero sí hay evento onclick con
+      //parámetro incluido: dni de esa tupla
+      " href='javascript:void(0)' onclick=eliminar(this,'" +
+      calculoPrecio +
+      "')>" +
+      //texto del botón e icono
+      "ELIMINAR<i class='bi-trash'></i></a></div> " +
+      "</td>";
+  }
+  contenedorCesta.appendChild(cajaTr);
+}
+
+function eliminar(fila, calculo) {
+  //Subir de nivel hasta llegar a elmento padre tabla
+  let filaTabla = fila.parentNode.parentNode;
+  //Subir un nivel más para coseguir el elemento tr de esa tabla y pasamos la tabla por parametro
+  filaTabla.parentNode.remove(filaTabla);
+  total = total - calculo;
+  precioTotal.textContent = total + "€";
+}
+
+function login() {
+  if (pwd.value == "frutas") {
+    window.open("../CRUD_Back/index.html");
+  }
+}
