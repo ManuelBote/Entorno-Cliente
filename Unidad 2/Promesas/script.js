b1c8442b11c4df06e717eb876fb1f7af3d7dbfc8
Manuel Bote <mbotez01@educarex.es>
2023-12-01 09:12:44 +0100

    Promesas Deportes

--- Unidad 2/Promesas/script.js
+++ Unidad 2/Promesas/script.js
@@ -1,10 +1,15 @@
 window.onload=inicio;
 
-let pais = document.querySelector("#Paises");
-let deporte = document.querySelector("#Deporte");
+let pais = document.querySelector("#paises");
+let deporte = document.querySelector("#deportes");
+let contenedorEquipo = document.querySelector("#equipos");
+let btn = document.querySelector("#mostrar");
 
 function inicio(){
     obtenerPaises();
+    obtenerDeportes();
+    let btn = document.querySelector("#mostrar");
+    btn.addEventListener("click", mostrarEquipos);
 }
 
 async function obtenerPaises(){
@@ -19,13 +24,60 @@ async function obtenerPaises(){
 
     try{
         const data = await objeto.json();
-        for(let i = 0; data.countries.length; i++){
+        for(let i = 0;i < data.countries.length; i++){
             pais.innerHTML+=
                 `<option value=${data.countries[i].name_en}>${data.countries[i].name_en}</option>`;
         }
     }
     
     catch(error){
-        //alert(error);
+        alert(error);
+    }
+}
+
+async function obtenerDeportes(){
+    const url = "deportes.json";
+    var headers= {};
+
+    const objeto = await fetch(url, {
+        method:"GET",
+        mode:"cors",
+        headers:headers
+    });
+
+    try{
+        const data = await objeto.json();
+        for(let i = 0;i <  data.sports.length; i++){
+            deporte.innerHTML+=
+                `<option value=${data.sports[i].strSport}>${data.sports[i].strSport}</option>`;
+        }
+    }
+    
+    catch(error){
+        alert(error);
+    }
+}
+
+async function mostrarEquipos(){
+    contenedorEquipo.innerHTML="";
+    const url = "https://www.thesportsdb.com/api/v1/json/3/search_all_teams.php?s="+deporte.value+"&c="+pais.value;
+    var headers= {};
+
+    const objeto = await fetch(url, {
+        method:"GET",
+        mode:"cors",
+        headers:headers
+    });
+
+    try{
+        const data = await objeto.json();
+        for(let i = 0;i <  data.teams.length; i++){
+            contenedorEquipo.innerHTML+=
+                `<div class="equipo"><img src="${data.teams[i].strTeamBadge}">${data.teams[i].strTeam}</div>`;
+        }
+    }
+    
+    catch(error){
+        alert(error);
     }
 }
